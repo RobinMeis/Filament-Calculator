fbfe55ea9fb6b40864a79d463fdcb09f8834739d
Robin Meis <robin.meis@smartnoob.de>
2014-12-26 18:39:20 +0100

    Komma durch Punkt ersetzen
    Wird ein Komma eingegeben, wird es durch einen Punkt ersetzt, damit
    Javascript mit den Daten rechnen kann

--- calc.js
+++ calc.js
@@ -7,6 +7,11 @@ function calc() {
 	});
 	
 	if (all_entered == true) {
+		$("#value_durchmesser").val($("#value_dichte").val().replace(",","."));
+		$("#value_durchmesser").val($("#value_durchmesser").val().replace(",","."));
+		$("#value_durchmesser").val($("#value_gewicht").val().replace(",","."));
+		$("#value_durchmesser").val($("#value_preis").val().replace(",","."));
+	
 		volumen = Math.PI * Math.pow(($("#value_durchmesser").val() / 2), 2) * $("#value_laenge").val();
 		gewicht = volumen / 1000000000 * $("#value_dichte").val() * 1000;
 		preis_pro_gramm = $("#value_preis").val() / $("#value_gewicht").val();
