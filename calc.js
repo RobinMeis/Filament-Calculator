9162a3ac57a97e97c3f3544a0eb983871ad2f036
Robin Meis <hc.devel@yahoo.de>
2015-01-05 18:02:21 +0100

    Bug Fix

--- calc.js
+++ calc.js
@@ -5,18 +5,18 @@ function calc() {
 			all_entered = false;
 		}
 	});
-	
+
 	if (all_entered == true) {
-		$("#value_durchmesser").val($("#value_dichte").val().replace(",","."));
+		$("#value_dichte").val($("#value_dichte").val().replace(",","."));
 		$("#value_durchmesser").val($("#value_durchmesser").val().replace(",","."));
-		$("#value_durchmesser").val($("#value_gewicht").val().replace(",","."));
-		$("#value_durchmesser").val($("#value_preis").val().replace(",","."));
-	
+		$("#value_gewicht").val($("#value_gewicht").val().replace(",","."));
+		$("#value_preis").val($("#value_preis").val().replace(",","."));
+
 		volumen = Math.PI * Math.pow(($("#value_durchmesser").val() / 2), 2) * $("#value_laenge").val();
 		gewicht = volumen / 1000000000 * $("#value_dichte").val() * 1000;
 		preis_pro_gramm = $("#value_preis").val() / $("#value_gewicht").val();
 		preis = preis_pro_gramm * gewicht;
-	
+
 		$("#calc_volumen").html(volumen.toFixed(2) + " mm&sup3;");
 		$("#calc_gewicht").html(gewicht.toFixed(2) + " g");
 		$("#calc_preis_pro_gramm").html(preis_pro_gramm + " &euro;");
